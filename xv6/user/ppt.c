820917829573a9c057594e3e601da445641138f4
king151dan <77873619+king151dan@users.noreply.github.com>
2026-02-06 17:15:20 -0800

    Add files via upload
    C source code and header files, assembly files, and configuration/build files. source code repository for xv6, a simple, educational operating system kernel based on Dennis Ritchie and Ken Thompson's Unix Version 6, but re-implemented for the modern RISC-V multiprocessor architecture using ANSI C.

---
+++ xv6/user/ppt.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+
+    printf("\n\tHello world (from Daniel hinga)\n");
+
+    prtpgtbl();
+
+    return 0;
+}
